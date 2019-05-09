0b5fe327e4c36aec78a0124988b3b3a8389b717d
Mukhtar Sibai <mukhtarsibai@hotmail.com>
2019-05-09 19:00:26 +0200

    Creating home component

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Home = () => (
-    <h1>Home Page</h1>
+      <div className="main">
+        <section className="intro">
+          <h1>X-Change</h1>
+          <p>Here you find rates for different currencies and can compare, amongst others, the rates are obtained from <b>European Central Bank.</b></p>
+          <p>You can get also the historical rates for any day since 1999.</p>
+        </section>
+      </div>
 );
 
 export default Home;
